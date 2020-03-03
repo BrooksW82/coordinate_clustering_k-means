81f0f210548ed7586ed075285e17928b912d2dae
BrooksW82 <35711130+BrooksW82@users.noreply.github.com>
2020-03-02 20:03:16 -0600

    Add files via upload

--- clusterAlgorithm.py
+++ clusterAlgorithm.py
@@ -1,3 +1,5 @@
+import math
+import random
 
 class ClusterAlgorithm:
 
@@ -7,10 +9,10 @@ class ClusterAlgorithm:
         f = open(text, "r")
         search_file = f.readline()
         self.k = int(search_file[search_file.find("k=")+2])
-        self.setArray(f)
+        self.set_array(f)
         f.close()
 
-    def setArray(self, file):
+    def set_array(self, file):
         for x, line in enumerate(file):
             if x > 0:
                 first_int = ""
@@ -26,8 +28,36 @@ class ClusterAlgorithm:
                         first_finished = True
                 self.coordinates.append([int(first_int), int(second_int), 0])
 
+    #creates a list of k amount of random points, where x-axis ranges between 0-1000 and y-axis ranges from 0-500
+    def cluster_algorithm(self):
+        ran_points = []
+        for x in range(self.k):
+            ran_points.append([random.randrange(1000),random.randrange(500)])
+        finished = False
+        while not finished:
+            for x in range(len(self.coordinates)):
+                farthest = 0
+                for i in range(self.k):
+                    dis = int(math.sqrt((self.coordinates[x][0] - ran_points[i][0])**2
+                                    + (self.coordinates[x][1] - ran_points[i][1])**2))
+                    if dis > farthest:
+                        self.coordinates[x][2] = i+1
+                        farthest = dis
 
+            for y in range(self.k):
+                new_point = []
+                new_x = 0
+                new_y = 0
+                sum   = 0
+                for j in range(len(self.coordinates)):
+                    if self.coordinates[j][2] == y+1:
+                        new_x += self.coordinates[j][0]
+                        new_y += self.coordinates[j][1]
+                        sum += 1
+                new_point.append([int(new_x/sum), int(new_y/sum)])
 
+            if new_point != ran_points:
+                
 
     def Print(self, file_name):
         f = open(file_name, "w")
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import clusterAlgorithm
 
 def main():
     test = clusterAlgorithm.ClusterAlgorithm("test.txt")
-
+    test.cluster_algorithm()
     for i in range(0,len(test.coordinates)):
-        test.Print("outputTest.txt")
+        print(test.coordinates[i])
+        #test.Print("outputTest.txt")
 main()
